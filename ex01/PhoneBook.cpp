7e8d9270cc001bd1641bac8439c6095cb6a3d0ca
dkhalil <dkhalil@localhost.localdomain>
2025-10-22 19:09:55 +0300

    updated

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: dikhalil <dikhalil@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 16:47:18 by dikhalil          #+#    #+#             */
-/*   Updated: 2025/10/22 18:51:52 by dikhalil         ###   ########.fr       */
+/*   Updated: 2025/10/22 19:08:45 by dikhalil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,35 +30,12 @@ bool PhoneBook::addContact()
         totalContacts++;
     return (true);
 }
-static int isNumber(const std::string &s)
-{
-    size_t i;
-
-    i = 0;
-    if (s.empty())
-        return (0);
-    while (std::isspace(s[i]))
-        i++;
-    if (s.length() == i)
-            return (0);
-    if (s[i] == '-' || s[i] == '+')
-    {
-        i++;
-        if (s.length() == i)
-            return (0);
-    }
-    for (size_t j = i; j < s.length(); j++)
-    {
-        if (!std::isdigit(s[j]))
-            return (0);
-    }
-    return (1);
-}
 
 bool PhoneBook::searchContacts() const
 {
     int     index;
     std::string input;
+    char *end;
 
     if (!totalContacts)
     {
@@ -73,9 +50,9 @@ bool PhoneBook::searchContacts() const
         std::cout << "\nEOF or error detected!" << std::endl;
         return (false);
     }
-    if (isNumber(input))
+    index = std::strtol(input.c_str(), &end, 10);
+    if (*end == '\0')
     {
-        index = atoi(input.c_str());
         if (index >= 1 && index <= totalContacts)
             contact[index - 1].getContact();
         else
